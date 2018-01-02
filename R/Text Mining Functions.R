e0e26dbb0654e8d137be3ceda579a846f60d5779
Chris - Home <jcjabr@gmail.com>
2018-01-02 00:23:56 -0600

    working on data + plot functions.

--- R/Text Mining Functions.R
+++ R/Text Mining Functions.R
@@ -97,6 +97,7 @@ TM.WordFrequency.Unnest <- function(df, text.col.string){
 }
 TM.WordFrequency.Unnest(series.2, "text")
 
+
 TM.WordFrequency.Groups.Unnest <- function(df, group.col.string, text.col.string){
   # insert: df, >= 1 grouping column, and word column. returns: grouped word counts
   require(dplyr); require(lazyeval); require(tidytext)
@@ -110,6 +111,34 @@ TM.WordFrequency.Groups.Unnest(series.2, "book", "text")
 TM.WordFrequency.Groups.Unnest(series, c("book","chapter"), "word")
 
 
+TM.WordFrequency.Groups.Unnest.Plot <- function(df, group.col.string, text.col.string, top.x = 10){
+  # insert: df, >= 1 grouping column, and word column. returns: grouped word counts
+  require(dplyr); require(lazyeval); require(tidytext); require(ggplot2)
+  df[[group.col.string]] <- as.factor(df[[group.col.string]])
+  df[[text.col.string]] <- as.character(df[[text.col.string]])
+  
+  full.data <- df %>%
+    unnest_tokens_("word", text.col.string) %>%
+    anti_join(stop_words) %>%
+    group_by_(.dots = group.col.string) %>%
+    count(word, sort = TRUE)
+
+  if (length(group.col.string) > 1) return(full.data)
+  
+  p <- full.data %>% top_n(top.x) %>% ungroup() %>% mutate(text_order = nrow(.):1) %>%
+    ggplot(aes(reorder(word, text_order), n, fill = word)) + geom_bar(stat = "identity") +
+    theme_bw() + 
+    labs(x = "Word", y = "Frequency") +
+    theme(legend.position = "none") +
+    facet_wrap(group.col.string, scales = "free_y") +
+    coord_flip()
+  
+  return(list(full.data = full.data, plot = p))
+}
+TM.WordFrequency.Groups.Unnest.Plot(series.2, "book", "text")
+TM.WordFrequency.Groups.Unnest.Plot(series, c("book","chapter"), "word")
+TM.WordFrequency.Groups.Unnest.Plot(series.2, "book", "text")
+str(TM.WordFrequency.Groups.Unnest.Plot(series.2, "book", "text"))
 
 
 
@@ -132,4 +161,36 @@ TM.WordUseByGroup <- function(df, group.col.string, text.col.string){
     ungroup()
 }
 TM.WordUseByGroup(series.2, "book", "text") %>% top_n(20) %>% View()
+
+TM.WordUseByGroup.Plot <- function(df, group.col.string, text.col.string, top.x = 20){
+  # percent of word use within group VS across entire population
+  require(dplyr); require(lazyeval); require(tidytext)
+  pct.group <- df %>%
+    unnest_tokens_("word", text.col.string) %>%
+    anti_join(stop_words) %>%
+    count(word) %>%
+    transmute(word, all_words = n / sum(n))
+  pct.all <- df %>%
+    unnest_tokens_("word", text.col.string) %>%
+    anti_join(stop_words) %>%
+    group_by(.dots = group.col.string) %>%
+    count(word) %>%
+    mutate(group_words = n / sum(n)) %>%
+    left_join(pct.group) %>%
+    arrange(desc(group_words)) %>%
+    ungroup()
+  p <- ggplot(pct.all, aes(x = group_words, y = all_words, color = abs(all_words - group_words))) +
+    geom_abline(color = "gray40", lty = 2) +
+    geom_jitter(alpha = 0.1, size = 2.5, width = 0.3, height = 0.3) +
+    geom_text(aes(label = word), check_overlap = TRUE, vjust = 1.5) +
+    scale_x_log10(labels = scales::percent_format()) +
+    scale_y_log10(labels = scales::percent_format()) +
+    scale_color_gradient(limits = c(0, 0.001), low = "darkslategray4", high = "gray75") +
+    facet_wrap(~ group.col.string, ncol = 2) +
+    theme(legend.position="none") +
+    labs(y = "Population Frequency", x = "Individual Group Frequency")
+  
+  return(list(data = pct.all, plot = p))
+}
+TM.WordUseByGroup.Plot(series.2, "book", "text")
 
